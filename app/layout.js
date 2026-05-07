419ef7d86fd3a249f6d341e82975e41a3900e8fb
Luis A. Casanova <132479713+LuisCasanova11@users.noreply.github.com>
2026-05-07 13:34:11 -0600

    tttttt

--- app/layout.js
+++ app/layout.js
@@ -3,3 +3,11 @@ export const metadata = {
   description: 'PWA con notificaciones push',
   manifest: '/manifest.json',
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
